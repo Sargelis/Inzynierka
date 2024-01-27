92d766a6a268436028a978fea6429fc2432e4567
Sarelis <pawel604@interia.eu>
2024-01-27 15:06:58 +0100

    UPDATE 27.01
    Added pause menu
    Buggs fixed

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -9,24 +8,34 @@ public class UIManager : MonoBehaviour
     public Button ab1, ab2, ab3;
     public TextMeshProUGUI text1, text2, text3;
     public Image image1, image2, image3;
-    //canvas ability
-    //timer
+    public Canvas ability;
+    public TextMeshProUGUI timer;
+    public Canvas pause;
+
+    GameManagment gameManager;
+
+    float minutes, seconds;
+    [HideInInspector] public float time;
+    bool isPaused = false;
+    [HideInInspector] public bool wasAbility = false;
 
     void Start()
     {
-        
+        ability.enabled = false;
+        pause.enabled = false;
+        minutes = 0f;
+        seconds = 0f;
+        time = 0f;
+        gameManager = FindObjectOfType<GameManagment>();
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && isPaused) Reseume();
+        else if (Input.GetKeyDown(KeyCode.Escape) && !isPaused) PauseMenu();
     }
-    //set timer
-    //set ability
-    //random ability
     //healthbar
     //expbar
     //coins
-    //pause menu
     public void SetBbutton1(int rand)
     {
         switch (rand) 
@@ -89,5 +98,56 @@ public class UIManager : MonoBehaviour
                 text3.text = "SCYTHE";
                 break;
         }
+    }
+    public void SetTimer()
+    {
+        time += 1 * Time.deltaTime;
+        minutes = Mathf.FloorToInt(time / 60);
+        seconds = Mathf.FloorToInt(time % 60);
+        timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    public void PauseMenu()
+    {
+        if (isPaused) Reseume();
+        else
+        {
+            if (wasAbility)
+            {
+                ability.enabled = false;
+                pause.enabled = false;
+                isPaused = false;
+            }
+            pause.enabled = true;
+            isPaused = true;
+            gameManager.Pause();
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Reseume()
+    {
+        if (wasAbility)
+        {
+            ability.enabled = true;
+            pause.enabled = false;
+            isPaused = false;
+            gameManager.Pause();
+        }
+        else
+        {
+            gameManager.UnPause();
+            pause.enabled = false;
+            isPaused = false;
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
